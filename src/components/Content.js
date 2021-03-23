b5439fb94a2f6e11da5fc2de164695004eeedcb9
Sam Sycamore <tangled@Sams-MacBook-Pro.local>
2021-03-22 17:35:45 -0700

    1.1

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </>
+  );
+};
+
+export default Content;
